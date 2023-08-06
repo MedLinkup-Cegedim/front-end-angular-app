9a66e4e2d23f205055fc4c49fc5afd09817cabc8
Abbas <y.abbas.habib@gmail.com>
2023-08-06 04:22:44 +0300

    Add sign-in page

---
+++ src/app/addcase/addcase.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addcase',
+  templateUrl: './addcase.component.html',
+  styleUrls: ['./addcase.component.css']
+})
+export class AddcaseComponent {
+
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AddcaseComponent } from './addcase/addcase.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+	{path:'', component:LoginComponent},
+	{path:'user/login', component:LoginComponent},
+	{path:'user/signup', component:SignupComponent},
+	{path:'user/addcase', component:AddcaseComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes), FlexLayoutModule ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
